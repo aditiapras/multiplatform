bcb4c24a4655033ce9211e25cb4da67f29205b2b
Aditia Prasetian <aditiapras97@gmail.com>
2023-11-06 19:38:46 +0700

    delete email

--- app/api/emails/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
-import nodemailer from "nodemailer";
-
-export async function POST(req) {
-  const { name, email, token } = await req.json();
-  return NextResponse.json({ name });
-}
