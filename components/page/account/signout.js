b6415c0ff272f3c70d3751ecc66ebcd9dad594e6
Aditia Prasetian <aditiapras97@gmail.com>
2023-11-09 01:39:59 +0700

    Update signout.js

--- components/page/account/signout.js
+++ components/page/account/signout.js
@@ -6,11 +6,8 @@ import { signOut } from "next-auth/react";
 export default function Logout() {
   return (
     <div>
-      <button
-        onClick={() => signOut()}
-        className="flex items-center bg-zinc-50 rounded-full border p-1 hover:bg-zinc-200"
-      >
-        <LogOut className="h-5 w-5" />
+      <button onClick={() => signOut()} className="flex items-center">
+        <LogOut className="h-6 w-6 hover:text-red-600" />
       </button>
     </div>
   );
