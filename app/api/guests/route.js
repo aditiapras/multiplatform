6e7563a07d62c14cc6e0e6bbc2b39246f6433757
Aditia Prasetian <aditiapras97@gmail.com>
2023-11-14 21:47:57 +0700

    Update route.js

--- app/api/guests/route.js
+++ app/api/guests/route.js
@@ -1,91 +1,85 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import moment from "moment-timezone";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient();
+const uid = new ShortUniqueId({ length: 8 });
 
 export async function POST(request) {
   const url = new URL(request.url);
   const id = url.searchParams.get("id");
   const type = url.searchParams.get("type");
 
-  const {
-    name,
-    address,
-    phoneNumber,
-    relationship,
-    invitationStatus,
-    wishes,
-    rsvp,
-  } = await request.json();
-
-  const data = await request.json();
-
-  data.map((data) => {
-    console.log(data);
-    return data;
-  });
-
-  return NextResponse.json("test");
-
-  // try {
-  //   if (type == "single") {
-  //     const data = await prisma.user.update({
-  //       where: {
-  //         userId: id,
-  //       },
-  //       data: {
-  //         guest: {
-  //           create: {
-  //             guestId: moment().unix(),
-  //             crated_at: moment().toISOString(),
-  //             updated_at: moment().toISOString(),
-  //             name,
-  //             address,
-  //             phoneNumber,
-  //             relationship,
-  //             invitationStatus,
-  //             wishes,
-  //             rsvp,
-  //           },
-  //         },
-  //       },
-  //     });
-  //     return NextResponse.json({ message: "Successfully added guest.", name });
-  //   } else if (type == "multiple") {
-  //     const data = await prisma.user.update({
-  //       where: {
-  //         userId: id,
-  //       },
-  //       data: {
-  //         guest: {
-  //           createMany: {
-  //             data: [
-  //               {
-  //                 guestId: moment().unix(),
-  //                 crated_at: moment().toISOString(),
-  //                 updated_at: moment().toISOString(),
-  //                 name,
-  //                 address,
-  //                 phoneNumber,
-  //                 relationship,
-  //                 invitationStatus,
-  //                 wishes,
-  //                 rsvp,
-  //               },
-  //             ],
-  //           },
-  //         },
-  //       },
-  //     });
-  //     return NextResponse.json({
-  //       message: "Successfully added multiple guests.",
-  //     });
-  //   }
-  // } catch (error) {
-  //   console.log(error);
-  //   return NextResponse.json({ message: "Cannot find user with this id" });
-  // }
+  try {
+    if (type == "single") {
+      const {
+        name,
+        address,
+        phoneNumber,
+        relationship,
+        invitationStatus,
+        wishes,
+        rsvp,
+      } = await request.json();
+      const data = await prisma.user.update({
+        where: {
+          userId: id,
+        },
+        data: {
+          guest: {
+            create: {
+              guestId: uid.rnd(),
+              crated_at: moment().toISOString(),
+              updated_at: moment().toISOString(),
+              name,
+              address,
+              phoneNumber,
+              relationship,
+              invitationStatus,
+              wishes,
+              rsvp,
+            },
+          },
+        },
+      });
+      return NextResponse.json({ message: "Successfully added guest.", name });
+    } else if (type == "multiple") {
+      const { data } = await request.json();
+      const query = data.map((item) => {
+        return {
+          guestId: uid.rnd(),
+          crated_at: moment().toISOString(),
+          updated_at: moment().toISOString(),
+          name: item.name,
+          address: item.address,
+          phoneNumber: item.phoneNumber,
+          relationship: item.relationship,
+          invitationStatus: item.invitationStatus,
+          wishes: item.wishes,
+          rsvp: item.rsvp,
+        };
+      });
+      const res = await prisma.user.update({
+        where: {
+          userId: id,
+        },
+        data: {
+          guest: {
+            createMany: {
+              data: query,
+            },
+          },
+        },
+      });
+      return NextResponse.json({
+        message: "Successfully added guest.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Cannot find user with this id" });
+  }
 }
 
 export async function PUT(request) {
